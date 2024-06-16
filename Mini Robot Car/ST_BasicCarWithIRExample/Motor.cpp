c73eb3437234d668764da055c3d2f202f4546169
Sungtaek <51244671+sungjuGit@users.noreply.github.com>
2024-06-16 10:05:23 -0700

    Add files via upload

---
+++ Mini Robot Car/ST_BasicCarWithIRExample/Motor.cpp
@@ -0,0 +1,25 @@
+#include <Arduino.h>
+#include "Motor.h"
+
+void Motor::Pin_init()
+{
+  pinMode(AIN1, OUTPUT);
+  pinMode(BIN1, OUTPUT);
+  pinMode(PWMA_LEFT, OUTPUT);
+  pinMode(PWMB_RIGHT, OUTPUT);
+  pinMode(STBY_PIN, OUTPUT);
+  digitalWrite(STBY_PIN, HIGH);
+}
+
+void Motor::Stop()
+{
+  analogWrite(PWMA_LEFT, 0);
+  analogWrite(PWMB_RIGHT, 0);
+}
+
+
+void Motor::Control(int PIN, int PIN_value, int PWM_pin, int speed)
+{
+  digitalWrite(PIN, PIN_value);
+  analogWrite(PWM_pin, speed);
+}
