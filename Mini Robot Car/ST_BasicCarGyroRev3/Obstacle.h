0b6d74edb14dc5209c1efca8c9ed304074a84551
Sungtaek <51244671+sungjuGit@users.noreply.github.com>
2024-08-06 10:29:47 -0700

    Add files via upload

---
+++ Mini Robot Car/ST_BasicCarGyroRev3/Obstacle.h
@@ -0,0 +1,60 @@
+/*
+ * @Description: Obstacle.h
+ * @Author: 
+ * @Date: 
+ * @LastEditTime: 
+ * @LastEditors: 
+ */
+ 
+#ifndef _OBSTACLE_H_
+#define _OBSTACLE_H_
+
+#include <Arduino.h>
+#include "Balanced.h"
+
+#define Distance_MIN 3
+#define Distance_MAX 100
+
+#define DISTANCE_JUDGEMENT (distance_value > Distance_MIN && distance_value < Distance_MAX)
+
+extern int obstacle_min;
+extern int turning_time;
+
+class Ultrasonic
+{
+  public:
+    void Pin_init();
+    void Get_Distance();
+    
+    static void Distance_Measure();
+
+  public:
+    static char measure_flag;
+    static unsigned long measure_prev_time;
+    unsigned long get_distance_prev_time;
+    static double distance_value;
+
+};
+
+
+class Function
+{
+  public:
+    void Obstacle_Mode();
+    void Maze_Mode();
+    void Left90Turn();
+    void Right90Turn();
+
+  public:
+    unsigned long obstacle_prev_time;
+    unsigned long Turning_time_counter;
+    unsigned long Obstacle_time;
+    unsigned long Back_time;
+    int turn_flag = 0;
+};
+
+
+
+
+
+#endif
---
+++ Mini Robot Car/ST_BasicCarGyroRev3/ST_BasicCarGyroRev3.ino
@@ -0,0 +1,94 @@
+/*
+ * @Description: ST_BasicCarExample.ino
+ * @Author: 
+ * @Date: 
+ * @LastEditTime: 
+ * @LastEditors: 
+ */
+ 
+#include "Motor.h"
+#include "Balanced.h"
+#include "Obstacle.h"
+#include "Pins.h"
+
+MyMpu6050 mympu6050;
+Motor mymotor;
+Ultrasonic myultrasonic;
+Balanced myBalanced;
+Function myfunction;
+Timer2 myTimer2;
+
+KalmanFilter kalmanfilter;
+
+int f_speed = 30, b_speed = 30, t_speed = 30;
+int obstacle_min = 30;  // in cm
+
+unsigned long start_prev_time = 0;
+
+
+void setup() 
+{
+  
+  mymotor.Pin_init(); 
+  mymotor.Encoder_init();
+   
+  mympu6050.init();
+  mympu6050.SetOffset(-5321, 874, 920, -19, 13, 23);  // these offset values should come from your MPU6050 calibration
+  
+  myultrasonic.Pin_init();
+
+  Serial.begin(9600);
+
+  start_prev_time = millis();
+  
+  myBalanced.Motion_Control(STOP);
+  
+  myTimer2.init(TIMER);
+
+  delay(1000); 
+  mympu6050.z_angle = 0;  // reset z_angle
+
+  
+  myBalanced.Motion_Control(FORWARD);
+  delay(1000);
+  
+  myfunction.Left90Turn();
+  delay(1000);
+  
+}
+
+
+
+void loop() 
+{ 
+     
+   //myfunction.Maze_Mode();
+
+
+  if (Serial.available())
+  {
+    
+    Serial.print("received");
+    char data = Serial.read();
+    Serial.print("data:"); Serial.println(data);
+    
+    if (data != '\0' && data != '\n')
+    {
+      switch (data)
+      {
+        case '1': myBalanced.Motion_Control(FORWARD); delay(1000); myBalanced.Motion_Control(STOP); break;
+
+        case '2': myBalanced.Motion_Control(BACK); delay(1000); myBalanced.Motion_Control(STOP); break;
+
+        case '3': myfunction.Left90Turn(); break;
+
+        case '4': myfunction.Right90Turn(); break;
+
+        default: myBalanced.Motion_Control(STOP);
+      }
+    }
+    
+  }
+
+   
+}
